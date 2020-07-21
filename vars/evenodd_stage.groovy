b18fe1269a3cad5d87b095c9b47f1571bd5bfe0b
Anuradha-bada <59429885+Anuradha-bada@users.noreply.github.com>
2020-07-21 14:32:48 +0530

    Update evenodd_stage.groovy

--- vars/evenodd_stage.groovy
+++ vars/evenodd_stage.groovy
@@ -1,25 +1,8 @@
 def call(int buildNumber) {
   if (buildNumber % 2 == 0) {
-    pipeline {
-      agent any
-      stages {
-        stage('Even Stage') {
-          steps {
-            echo "The build number is even"
-          }
-        }
-      }
-    }
-  } else {
-    pipeline {
-      agent any
-      stages {
-        stage('Odd Stage') {
-          steps {
-            echo "The build number is odd"
-          }
-        }
-      }
-    }
+    echo "current build is even build stage"
+  }
+  else{
+    echo "current build is odd build stage"
   }
 }
